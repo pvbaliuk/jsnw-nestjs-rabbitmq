d61a5ae4c57904643beffaec8b26079b56a0f040
Pavlo Baliuk <pv.baliuk@gmail.com>
2026-07-20 08:53:13 +0300

    Fix RabbitmqStorage

--- src/nestjs/rabbitmq-storage.ts
+++ src/nestjs/rabbitmq-storage.ts
@@ -1,50 +1,37 @@
-import {AMQP_ENTITY_KIND_TOKEN, type AMQPExchange, type AMQPQueue} from '../dsl';
+import type {AnyRMQExchange, AnyRMQQueue} from '../dsl';
 
 export class RabbitmqStorage{
 
-    private static exchanges = new Set<AMQPExchange<any>>;
-    private static queues = new Set<AMQPQueue<any, any>>;
+    private static exchanges = new Set<AnyRMQExchange>;
+    private static queues = new Set<AnyRMQQueue>;
 
     /**
-     * @param {AMQPExchange<any>[]} exchanges
+     * @param {AnyRMQExchange[]} exchanges
      */
-    public static addExchanges(exchanges: AMQPExchange<any>[]): void{
+    public static addExchanges(exchanges: AnyRMQExchange[]): void{
         for(const exchange of exchanges)
             this.exchanges.add(exchange);
     }
 
     /**
-     * @param {AMQPQueue<any, any>[]} queues
+     * @param {AnyRMQQueue[]} queues
      */
-    public static addQueues(queues: AMQPQueue<any, any>[]): void{
+    public static addQueues(queues: AnyRMQQueue[]): void{
         for(const queue of queues)
             this.queues.add(queue);
     }
 
     /**
-     * @param {(AMQPExchange<any> | AMQPQueue<any, any>)[]} items
+     * @return {AnyRMQExchange[]}
      */
-    public static addAny(items: (AMQPExchange<any>|AMQPQueue<any, any>)[]): void{
-        for(const item of items){
-            if(item[AMQP_ENTITY_KIND_TOKEN] === 'exchange'){
-                this.exchanges.add(item);
-            }else{
-                this.queues.add(item);
-            }
-        }
-    }
-
-    /**
-     * @return {AMQPExchange<any>[]}
-     */
-    public static getExchanges(): AMQPExchange<any>[]{
+    public static getExchanges(): AnyRMQExchange[]{
         return Array.from(this.exchanges);
     }
 
     /**
-     * @return {AMQPQueue<any, any>[]}
+     * @return {AnyRMQQueue[]}
      */
-    public static getQueues(): AMQPQueue<any, any>[]{
+    public static getQueues(): AnyRMQQueue[]{
         return Array.from(this.queues);
     }
 
